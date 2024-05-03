3d5349c1a5c37f512429fcd73a0ac2b31f9aa045
lbutkovich <lbutkovich@ucsb.edu>
2024-05-02 21:58:10 -0700

    Add .env file to .gitignore and update Script_1_MZmine3_workflow.py to load environment variables from .env file

--- Script_1_MZmine3_workflow.py
+++ Script_1_MZmine3_workflow.py
@@ -4,7 +4,8 @@ LCMSMS Data Analysis Workflow, Script 1: Prepare Inputs for MZmine3
 @author: Lazarina Butkovich
 
 Features include:
-    Create metadata .tsv file in temp folder
+
+
 """""""""""""""""""""""""""""""""""""""""""""
 import pandas as pd
 import os
@@ -13,6 +14,8 @@ import xml.etree.ElementTree as ET
 import subprocess
 import shutil
 import ftplib
+# Install python-dotenv
+from dotenv import load_dotenv
 
 """""""""""""""""""""""""""""""""""""""""""""
 Functions
@@ -199,10 +202,11 @@ MZMINE_EXE_DIR = 'D:\MZmine\MZmine.exe'
 # GNPS FTP server: massive.ucsd.edu (check GNPS documentation if this ever changes)
 HOSTNAME = 'massive.ucsd.edu'
 ALL_UPLOADS_FOLDER_NAME = "GNPS_upload_from_MZmine3_script_1"
-# later edit these to be encoded in a separate (not public) file
-USERNAME = 'lbutkovich'
-PASSWD = 'password123'
-
+# Load .env file
+load_dotenv()
+# Get USERNAME and PASSWD from .env file
+USERNAME = os.getenv('USERNAME')
+PASSWD = os.getenv('PASSWD')
 
 """""""""""""""""""""""""""""""""""""""""""""
 Create GNPS and MetaboAnalyst Metadata .tsv files
@@ -233,10 +237,14 @@ for filename in os.listdir(temp_folder):
         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
-# Format of metadata .tsv: two columns, Filename and Class. List the filenames from folder Job Name, where filenames with 'CTRL' in them are in the 'CTRL' class and filenames without 'CTRL' in them are in the 'EXP' class.
+# Format of MZmine3 metadata .tsv: two columns, Filename and Class. List the filenames from folder Job Name, where filenames with 'CTRL' in them are in the 'CTRL' class and filenames without 'CTRL' in them are in the 'EXP' class.
 # Create metadata .tsv file in temp folder
-metadata_filename = job_name + '_metadata.tsv'
-metadata_filepath = pjoin(temp_folder, metadata_filename)
+# Format of GNPS metadata .tsv: same as MZmine3 except header is "filename" and "ATTRIBUTE_Group"
+metadata_filename_mzmine3 = job_name + '_metadata.tsv'
+metadata_filepath_mzmine3 = pjoin(temp_folder, metadata_filename_mzmine3)
+metadata_filename_gnps = job_name + '_metadata_gnps.tsv'
+metadata_filepath_gnps = pjoin(temp_folder, job_name + '_metadata_gnps.tsv')
+
 # Use pandas to create metadata .tsv file
 metadata_df = pd.DataFrame(columns = ['Filename', 'Class'])
 
@@ -261,7 +269,12 @@ exp_filenames = [filename for filename in mzml_filenames if filename.endswith('.
 # Add filenames to metadata_df
 metadata_df['Filename'] = ctrl_filenames + exp_filenames
 metadata_df['Class'] = ['CTRL']*len(ctrl_filenames) + ['EXP']*len(exp_filenames)
-metadata_df.to_csv(metadata_filepath, sep = '\t', index = False)
+metadata_df.to_csv(metadata_filepath_mzmine3, sep = '\t', index = False)
+
+# Create metadata .tsv file for GNPS by adjusting MetaboAnalyst metadata .tsv file
+metadata_df_gnps = metadata_df.copy()
+metadata_df_gnps.columns = ['filename', 'ATTRIBUTE_Group']
+metadata_df_gnps.to_csv(metadata_filepath_gnps, sep = '\t', index = False)
 
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -295,9 +308,9 @@ change_node_parameters(xml_root, 'batchstep[@method="io.github.mzmine.modules.io
 Edit basic .xml parameters file: metadata
 """
 # Update current_file for metadata section
-change_node_parameters(xml_root, 'batchstep[@method="io.github.mzmine.modules.visualization.projectmetadata.io.ProjectMetadataImportModule"][@parameter_version="1"]', 'parameter[@name="File names"]', 'current_file', [xml_temp_dir_pre_str + job_name + '\\' + metadata_filename])
+change_node_parameters(xml_root, 'batchstep[@method="io.github.mzmine.modules.visualization.projectmetadata.io.ProjectMetadataImportModule"][@parameter_version="1"]', 'parameter[@name="File names"]', 'current_file', [xml_temp_dir_pre_str + job_name + '\\' + metadata_filename_mzmine3])
 # Update last_file for metadata section
-change_node_parameters(xml_root, 'batchstep[@method="io.github.mzmine.modules.visualization.projectmetadata.io.ProjectMetadataImportModule"][@parameter_version="1"]', 'parameter[@name="File names"]', 'last_file', [xml_temp_dir_pre_str + job_name + '\\' + metadata_filename])
+change_node_parameters(xml_root, 'batchstep[@method="io.github.mzmine.modules.visualization.projectmetadata.io.ProjectMetadataImportModule"][@parameter_version="1"]', 'parameter[@name="File names"]', 'last_file', [xml_temp_dir_pre_str + job_name + '\\' + metadata_filename_mzmine3])
 
 
 """
@@ -322,19 +335,20 @@ xml_gnps_child = xml_root.find('batchstep[@method="io.github.mzmine.modules.io.e
 xml_gnps_autorun_node = xml_gnps_child.find('parameter[@name="Submit to GNPS"]')
 # xml_method_gnps_autorun_node.set('selected', 'true')
 
-# Adjust metadata file for GNPS job auto-run
-xml_gnps_autorun_metadata_subnode = xml_gnps_autorun_node.find('parameter[@name="Meta data file"]')
-# Within this node, adjust current_file and last_file
+# Adjust metadata file for GNPS job auto-run. Use metadata specific for GNPS
+xml_gnps_autorun_metadata_subnode = xml_gnps_autorun_node.find('parameter[@name="Metadata file"]')
+for filename in xml_gnps_autorun_metadata_subnode.findall('current_file'):
+    xml_gnps_autorun_metadata_subnode.remove(filename)
 current_file = ET.Element('current_file')
 for filename in xml_gnps_autorun_metadata_subnode.findall('current_file'):
     xml_gnps_autorun_metadata_subnode.remove(filename)
-current_file.text = xml_temp_dir_pre_str + job_name + '\\' + metadata_filename
+current_file.text = xml_temp_dir_pre_str + job_name + '\\' + metadata_filename_gnps
 xml_gnps_autorun_metadata_subnode.append(current_file)
 
 last_file = ET.Element('last_file')
 for filename in xml_gnps_autorun_metadata_subnode.findall('last_file'):
     xml_gnps_autorun_metadata_subnode.remove(filename)
-last_file.text = xml_temp_dir_pre_str + job_name + '\\' + metadata_filename
+last_file.text = xml_temp_dir_pre_str + job_name + '\\' + metadata_filename_gnps
 xml_gnps_autorun_metadata_subnode.append(last_file)
 
 # Adjust job title for GNPS job auto-run
@@ -351,7 +365,8 @@ xml_method_gnps_autorun_username_node.text = 'lbutkovich'
 
 # Adjust password for GNPS job auto-run
 xml_method_gnps_autorun_password_node = xml_gnps_autorun_node.find('parameter[@name="Password"]')
-xml_method_gnps_autorun_password_node.text = 'password123'
+# Get password from .env file
+xml_method_gnps_autorun_password_node.text = os.getenv('PASSWD')
 
 
 """
@@ -413,8 +428,7 @@ shutil.move(pjoin(temp_folder, job_name + '_gnps.mgf'), pjoin(gnps_input_folder,
 Metadata .tsv file
 """
 # Copy the metadata .tsv from temp folder, job_name folder to the GNPS_input_for_job_name folder
-shutil.copy(metadata_filepath, pjoin(gnps_input_folder, metadata_filename))
-""
+shutil.copy(metadata_filepath_gnps, pjoin(gnps_input_folder, metadata_filename_gnps))
 
 """""""""""""""""""""""""""""""""""""""""""""
 Use FTP () to upload GNPS_input_for_job_name folder to GNPS
