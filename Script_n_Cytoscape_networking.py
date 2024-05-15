84e6648594e7bbbc3b7a6dcefae7b84e9f29d8fb
lbutkovich <lbutkovich@ucsb.edu>
2024-05-14 17:54:28 -0700

    Script Cytoscape, import MetaboAnalyst data and align data columns to nodes table

--- Script_n_Cytoscape_networking.py
+++ Script_n_Cytoscape_networking.py
@@ -26,6 +26,17 @@ cytoscape_inputs_folder_name = 'Cytoscape_inputs'
 # Cytoscape style .xml filename (located in cytoscape_inputs_folder)
 cytoscape_style_filename = 'styles_6.xml'
 
+# MetaboAnalyst Outputs of Interest
+metaboanalyst_output_folder_name = 'MetaboAnalystR_Output'
+metaboanalyst_log2FC_filename_post_str = "_fold_change.csv"
+metaboanalyst_ttest_filename_post_str = "_t_test.csv"
+metaboanalyst_norm_peak_area_filename_post_str = "_normalized_data_transposed.csv"
+
+# Columns from MetaboAnalyst Outputs to add to Cytoscape node table
+log2fc_cols_to_keep = ['shared_name', 'log2.FC.']
+t_test_cols_to_keep = ['shared_name', 'p.value']
+
+
 """""""""""""""""""""""""""""""""""""""""""""
 Get Job Info
 """""""""""""""""""""""""""""""""""""""""""""
@@ -48,8 +59,10 @@ cytoscape_inputs_folder = pjoin(INPUT_FOLDER, 'Cytoscape_inputs')
 """""""""""""""""""""""""""""""""""""""""""""
 Open Cytoscape network from GNPS output .graphml
 """""""""""""""""""""""""""""""""""""""""""""
-# GNPS Outputs are located in temp_folder
+# Destroy any networks already in the Cytsocape session
+p4c.networks.delete_all_networks()
 
+# GNPS Outputs are located in temp_folder
 # Find the .graphml file in the GNPS output folder. Upload into Cytoscape
 gnps_graphml_file = [f for f in os.listdir(gnps_output_folder) if f.endswith('.graphml')][0]
 
@@ -58,6 +71,48 @@ p4c.import_network_from_file(pjoin(gnps_output_folder, gnps_graphml_file))
 
 # Assuming 'network_suid' is the SUID of the network you want to rename
 p4c.networks.rename_network(job_name)
+
+# Get the SUID of the network
+network_suid = p4c.networks.get_network_suid(job_name)
+
+
+"""""""""""""""""""""""""""""""""""""""""""""
+Import MetaboAnalyst Data
+"""""""""""""""""""""""""""""""""""""""""""""
+# Import log2fc data. Values >0 are upregulated in EXP.
+log2fc_filename = job_name + metaboanalyst_log2FC_filename_post_str
+log2fc_data = pd.read_csv(pjoin(temp_job_folder, metaboanalyst_output_folder_name, log2fc_filename))
+
+# Import t-test data. Values <0.05 are significant.
+t_test_filename = job_name + metaboanalyst_ttest_filename_post_str
+t_test_data = pd.read_csv(pjoin(temp_job_folder, metaboanalyst_output_folder_name, t_test_filename))
+
+# Import normalized peak area data
+norm_peak_area_filename = job_name + metaboanalyst_norm_peak_area_filename_post_str
+norm_peak_area_data = pd.read_csv(pjoin(temp_job_folder, metaboanalyst_output_folder_name, norm_peak_area_filename))
+
+# Indicate which columns to keep in the imported MetaboAnalyst data
+log2fc_data = log2fc_data[log2fc_cols_to_keep]
+t_test_data = t_test_data[t_test_cols_to_keep]
+# Keep all normalized peak area columns except for MetaboAnalyst_ID
+norm_peak_area_data = norm_peak_area_data.drop(columns=['MetaboAnalyst_ID'])
+
+# Rename normalized data columns (not shared_name) to end with '_normalized'
+norm_peak_area_data.columns = [col + '_normalized' if col != 'shared_name' else col for col in norm_peak_area_data.columns]
+
+# For all tables, change data type of 'shared_name' column to strings, in order to match the data type in the Cytoscape network
+log2fc_data['shared_name'] = log2fc_data['shared_name'].astype(str)
+t_test_data['shared_name'] = t_test_data['shared_name'].astype(str)
+norm_peak_area_data['shared_name'] = norm_peak_area_data['shared_name'].astype(str)
+
+# Line up the data against the nodes in the network
+# 'shared name' is the key column that we will use to align the data. table='node' by default. table_key_column='name' by default, and 'name' is the same as 'shared name'
+p4c.tables.load_table_data(log2fc_data, data_key_column='shared_name',network=network_suid)
+p4c.tables.load_table_data(t_test_data, data_key_column='shared_name',network=network_suid)
+p4c.tables.load_table_data(norm_peak_area_data, data_key_column='shared_name',network=network_suid)
+
+# print data type of values in log2fc_data column shared_name
+print(log2fc_data['shared_name'].dtype)
 
 """""""""""""""""""""""""""""""""""""""""""""
 Set Visual Style
@@ -71,5 +126,8 @@ cytoscape_style_name = cytoscape_style_filename.split('.')[0]
 # Set the visual style to cystocape_style_filename, without the file extension in the cytoscape_style_filename
 p4c.set_visual_style(cytoscape_style_name)
 
-# to-do: import MetaboAnalyst outputs to align
+
+
+
+
 
